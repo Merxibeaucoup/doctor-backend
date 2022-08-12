d04abe5405049ea934972847a11d70559a8f679c
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2022-08-12 19:40:55 -0400

    please

--- src/main/java/com/edgar/dentist/DentistAppApplication.java
+++ src/main/java/com/edgar/dentist/DentistAppApplication.java
@@ -2,9 +2,10 @@ package com.edgar.dentist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DentistAppApplication {
+public class DentistAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DentistAppApplication.class, args);
